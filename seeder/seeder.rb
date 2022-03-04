6213f000f4df486a62ea280c6ce6ab143d2f2f77
yastamardika <rattyasta1@gmail.com>
2022-03-05 03:45:47 +0700

    add config and seeder

---
+++ seeder/seeder.rb
@@ -0,0 +1,8 @@
+pages << {
+    page_type: "listings",
+    method: "GET",
+    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+    url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
+    fetch_type: "browser"
+  }
+  
