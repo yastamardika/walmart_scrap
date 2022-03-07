8fa95d27a03f012efe2a11e40f72060d04ca99b2
yastamardika <rattyasta1@gmail.com>
2022-03-08 00:33:11 +0700

    bypass captcha and fix listing parser

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -1,14 +1,30 @@
 html = Nokogiri.HTML(content)
 
-products = html
-p products
+products = html.css('div[data-stack-index="0"] section div.flex.flex-wrap div.mb1.ph1.pa0-xl')
+# p products
 products.each do |product|
-  href = product.at_css('a.product-title-link')['href']
-  url = URI.join('https://www.walmart.com', href).to_s
+  href = product.css('a.absolute.w-100.h-100.z-1 > @href').text
+  # p href
+  url = "https://www.walmart.com#{href}"
   pages << {
       url: url,
       page_type: 'products',
       fetch_type: 'browser',
       vars: {}
     }
+end
+
+pagination_links = html.css('.list.flex.items-center.justify-center.pa0 li')
+next_ur = pagination_links.css('a')[4].text.to_i
+i = 2
+p next_ur
+while i <= next_ur
+  url = "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases%3ALast+90+Days&page=#{i}&affinityOverride=default"
+  pages << {
+      url: url,
+      page_type: 'listings',
+      fetch_type: 'browser',
+      vars: {}
+    }
+  i += 1
 end
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,13 +2,26 @@ pages << {
     page_type: "listings",
     method: "GET",
     headers: {
-        "User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
-        "Sec-CH-UA" => "'Not A;Brand';v='99', 'Chromium';v='96', 'Google Chrome';v='96'"},
+        "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
+        "Sec-CH-UA" => "'Not A;Brand';v='99', 'Chromium';v='96', 'Google Chrome';v='96'",
+        "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding" => "gzip, deflate, br",
+        "Accept-Language" => "en-US,en;q=0.9,id;q=0.8",
+        "Sec-Ch-Ua-Arch" => "x86",
+        "Sec-Ch-Ua-Platform" => "Windows"
+    },
     url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
     fetch_type: "browser",
-    display: {
-        "width": 1366,
-        "height": 768
+    driver: {
+        pre_code: "await page.setViewport({
+            width: 1920 + Math.floor(Math.random() * 100),
+            height: 3000 + Math.floor(Math.random() * 100),
+            deviceScaleFactor: 1,
+            hasTouch: false,
+            isLandscape: false,
+            isMobile: false,
+        });",
+        disable_adblocker: true,
     }
     
   }
