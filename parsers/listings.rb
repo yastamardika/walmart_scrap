7a005dd9ed021166ed04fcd6714f7503d22c3890
yastamardika <rattyasta1@gmail.com>
2022-03-09 23:36:28 +0700

    fix listings parser header

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -14,11 +14,6 @@ products.each do |product|
       vars: {},
       headers: {
         "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
-        "Sec-CH-UA" => "'Not A;Brand';v='99', 'Chromium';v='96', 'Google Chrome';v='96'",
-        "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "Accept-Encoding" => "gzip, deflate, br",
-        "Accept-Language" => "en-US,en;q=0.9,id;q=0.8",
-        "Sec-Ch-Ua-Arch" => "x86",
         "Sec-Ch-Ua-Platform" => "Windows"
       },
       driver: {
@@ -47,11 +42,6 @@ if next_ur != []
       fetch_type: 'browser',
       headers: {
         "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
-        "Sec-CH-UA" => "'Not A;Brand';v='99', 'Chromium';v='96', 'Google Chrome';v='96'",
-        "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "Accept-Encoding" => "gzip, deflate, br",
-        "Accept-Language" => "en-US,en;q=0.9,id;q=0.8",
-        "Sec-Ch-Ua-Arch" => "x86",
         "Sec-Ch-Ua-Platform" => "Windows"
       },
       driver: {
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -3,9 +3,6 @@ pages << {
     method: "GET",
     headers: {
         "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
-        "Sec-CH-UA" => '"Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
-        "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "Sec-Ch-Ua-Arch" => "x86",
         "Sec-Ch-Ua-Platform" => "Windows"
     },
     url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
