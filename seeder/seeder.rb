0a81f6731e117d89a8e8775111835e82015ec88a
yastamardika <rattyasta1@gmail.com>
2022-03-09 23:52:02 +0700

    change header

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -3,7 +3,8 @@ pages << {
     method: "GET",
     headers: {
         "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
-        "Sec-Ch-Ua-Platform" => "Windows"
+        "Sec-Ch-Ua-Platform" => "Windows",
+        "Sec-CH-UA" => '"Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
     },
     url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
     fetch_type: "browser",
@@ -21,7 +22,7 @@ pages << {
     screenshot: {
         "take_screenshot": true,
         "options": {
-         "fullPage": false,
+         "fullPage": true,
          "type": "jpeg",
          "quality": 75
        }
