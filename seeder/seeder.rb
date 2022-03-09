4f54697265741ea2fa282774fa60b95c8f74b183
yastamardika <rattyasta1@gmail.com>
2022-03-10 00:42:02 +0700

    modify seeder header

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,14 +2,15 @@ pages << {
     page_type: "listings",
     method: "GET",
     headers: {
-        "Accept" => "application/json",
-        "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
-        "Sec-Ch-Ua-Platform" => "Windows",
-        "Sec-CH-UA" => '"Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
-        "Accept-Encoding" => "gzip, deflate, br",
-        "Accept-Language" => '"en-US,en;q=0.9,id;q=0.8"',
-        "Sec-Ch-Ua-Arch" => "x86",
-        "Sec-Ch-Ua-Platform" => "Windows"
+        "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "sec-ch-ua" => '"Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+        "sec-ch-ua-mobile"=> "?0",
+        "sec-ch-ua-platform"=> '"Linux"',
+        "sec-fetch-dest"=> "document",
+        "sec-fetch-mode"=> "navigate",
+        "sec-fetch-site" => "same-origin",
+        "sec-fetch-user" => "?1",
+        "upgrade-insecure-requests" => "1"
     },
     url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
     fetch_type: "browser",
